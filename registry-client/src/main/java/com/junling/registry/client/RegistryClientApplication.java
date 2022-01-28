c1e32f0c45dd3f3b384af6a509efc7a188329153
junlingsun <junlingsun1983@gmail.com>
2022-01-28 12:17:01 -0800

    init project

---
+++ registry-client/src/main/java/com/junling/registry/client/RegistryClientApplication.java
@@ -0,0 +1,13 @@
+package com.junling.registry.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryClientApplication.class, args);
+	}
+
+}
